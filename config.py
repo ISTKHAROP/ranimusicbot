5fa6e1371e35aa82d2f79e016b9cae58182ae8c1
ISTKHAROP <istkharalam2006@gmail.com>
2025-08-16 17:40:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BADANAM001/ranimusicbot",
+    "lhttps://github.com/ISTKHAROP/ranimusicbot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
